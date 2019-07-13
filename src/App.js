942c1b375172cc33349e2e85137d6aa2eb7cc996
xaviguasch <xaviguasch@gmail.com>
2019-07-13 22:34:14 +0200

    added SignIn component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/shop/ShopPage'
+import SignInAndSignOut from './pages/sign-in-and-sign-out/SingInAndSignOut'
 import Header from './components/header/Header'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignOut} />
       </Switch>
     </div>
   )
---
+++ src/pages/sign-in-and-sign-out/SingInAndSignOut.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/SignIn'
+
+import './SingInAndSignOut.scss'
+
+const SingInAndSignOut = () => {
+  return (
+    <div className='sign-in-and-sign-out'>
+      <SignIn />
+    </div>
+  )
+}
+
+export default SingInAndSignOut
