c83d4629b62caed1b96d402292380bc1f4ec4ac1
xaviguasch <xaviguasch@gmail.com>
2019-10-26 18:05:08 +0200

    stored user data in Firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/shop/ShopPage'
 import SignInAndSignOut from './pages/sign-in-and-sign-out/SingInAndSignOut'
 import Header from './components/header/Header'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends Component {
   constructor(props) {
@@ -19,10 +19,8 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user)
     })
   }
 
--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -8,7 +8,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
       className={`${size} menu-item`}
       onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
-      <div style={{ backgroundImage: `url(${imageUrl})` }} className='background-image' />
+      <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
