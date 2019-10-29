ddb2b23a210af42140573121bfc22927357eca95
xaviguasch <xaviguasch@gmail.com>
2019-10-29 10:48:43 +0100

    added CartDropdown component

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
-import './cart-icon.scss'
+import './CartIcon.scss'
 
 const CartIcon = () => (
   <div className='cart-icon'>
